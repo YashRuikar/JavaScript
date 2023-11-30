1746f56e1ddcf3ff9e1d0213756401907aca35fa
YashRuikar <yash04ruikar@gmail.com>
2023-11-30 15:59:16 +0530

    Operations of data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -77,3 +77,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+
+// ******************* OPERATIONS *********************
+
+
+let str1 = "Hello"
+let str2 = " yash"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + "2");     // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32
+
+console.log(+true);  // 1
+console.log(+"");    // Error
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
