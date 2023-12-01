0d208481bee145856ee0e5ac2b2ce9fed5c28cb3
YashRuikar <yash04ruikar@gmail.com>
2023-12-01 10:27:58 +0530

    comparison between data types

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(3 > 2);
+// console.log(3 >= 2);
+// console.log(3 < 2);
+// console.log(3 <= 2);
+// console.log(3 == 2);
+// console.log(3 != 2);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+
+// Strict Check ===   It checks data types also
+
+console.log("2" === 2);
