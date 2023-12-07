ecb961311d003ddc1134f66818001dc864fd7583
YashRuikar <yashruikar04@gmail.com>
2023-12-07 10:49:40 +0530

    functions in js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -36,4 +36,45 @@ function loginUserMessage(username = "Sam"){
 
 // console.log(loginUserMessage("Yash"));
 // console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 500, 800, 2000));
+
+
+
+// Object passed in function
+
+const user = {
+    username: "Yash",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+
+// Array passed in function
+
+const myNewArray = [200, 400, 1000, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 1000, 600]));
+
