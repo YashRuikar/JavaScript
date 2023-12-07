77a05453f98c08feca9816329da310b8f6ac5a2c
YashRuikar <yashruikar04@gmail.com>
2023-12-07 12:11:38 +0530

    Scope level

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -4,9 +4,56 @@ let a = 200
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "yash"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+
+    // console.log(website); // Error
+
+    // two()
+}
+
+// one()
+
+
+
+if (true) {
+    const username = "Lionel Messi"
+
+    if (username === "Lionel Messi") {
+        const worldCup = " 2022 World Cup Winner"
+        // console.log(username + worldCup);
+    }
+
+    // console.log(worldCup);  // Error
+}
+
+// console.log(username);  // Error
+
+
+
+
+console.log(addOne(3))
+
+function addOne(num){
+    return num + 1
+}
+
+
+// addTwo(10) // Error
+
+const addTwo = function(num){
+    return num + 2
+}
