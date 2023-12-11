6e1650f9756ffd1bea451c796fb6a03e6cf3fa3b
YashRuikar <yashruikar04@gmail.com>
2023-12-11 15:41:45 +0530

    While & do while loops

---
+++ 05_Loops/while_loop.js
@@ -0,0 +1,26 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["Flash", "Batman", "Superman"]
+
+let a = 0
+while (a < myArray.length) {
+    console.log(`Value is ${myArray[a]}`);
+    a++
+}
+
+
+// do while loop 
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
